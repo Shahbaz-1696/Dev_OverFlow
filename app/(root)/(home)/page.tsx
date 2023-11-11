43b7581395654954dc18d1512af3f1bbad817dda
Shahbaz Ansari <ansarishahbaz96@gmail.com>
2023-11-11 23:27:10 +0530

    Updated folder Structure and added QuestionCard component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,52 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import LocalSearch from "@/components/shared/search/LocalSearch";
-import Filter from "@/components/shared/filter/Filter";
+import Filter from "@/components/shared/Filter";
 import HomeFilters from "@/components/home/HomeFilters";
 import { HomePageFilters } from "@/constants/filters";
+import NoResult from "@/components/shared/NoResult";
+import QuestionCard from "@/components/cards/QuestionCard";
+
+const questions = [
+  {
+    _id: 1,
+    title: `How do ES6 module exports and imports work in JavaScript, and what are the key differences from CommonJS?`,
+    tags: [
+      { _id: 1, name: "next.js" },
+      { _id: 2, name: "javascript" },
+    ],
+    author: {
+      _id: 1,
+      name: "Shahbaz",
+      picture: "url_to_picture_1",
+    },
+    upVotes: 10000000,
+    answers: [
+      /* array of answers here */
+    ],
+    views: 50000,
+    createdAt: new Date("2023-07-16"),
+  },
+  {
+    _id: 2,
+    title: `How to center a div?`,
+    tags: [
+      { _id: 1, name: "css" },
+      { _id: 2, name: "html" },
+    ],
+    author: {
+      _id: 2,
+      name: "Shadab",
+      picture: "url_to_picture_2",
+    },
+    upVotes: 10,
+    answers: [
+      /* array of answers here */
+    ],
+    views: 50,
+    createdAt: new Date("2023-09-23"),
+  },
+];
 
 export default function Home() {
   return (
@@ -33,6 +76,33 @@ export default function Home() {
         />
       </div>
       <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upVotes={question.upVotes}
+              answers={question.answers}
+              views={question.views}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There’s no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+          discussion. our query could be the next big thing others learn from. Get
+          involved!"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import RenderTag from "../renderTag/RenderTag";
+import RenderTag from "./RenderTag";
 
 const RightSidebar = () => {
   const topQuestions = [
